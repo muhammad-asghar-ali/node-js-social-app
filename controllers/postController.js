8ace733e27479f188eb8fb303c06599f2175fff1
unknown <alikhan137>
2022-09-26 22:23:22 -0700

    create upload image contorller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,11 @@
 const PostModel = require("../models/post")
+const cloudinary = require("cloudinary")
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
 
 module.exports.createdPost = async(req, res) => {
     try {
@@ -11,6 +18,18 @@ module.exports.createdPost = async(req, res) => {
         res.status(200).json({
             message: "Post is created",
             post: post
+        })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+module.exports.uploadImage = async(req, res) => {
+    try {
+        const result = await cloudinary.uploader.upload(req.files.image.path)
+        res.status(200).json({
+            url: result.secure_url,
+            public_id: result.public_id
         })
     } catch (err) {
         res.status(500).json({ message: err.message })
